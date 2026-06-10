358372c82cf65c5a24fd65c749845f227f34a202
nick <drevilbones@gmail.com>
2026-06-10 10:20:48 -0400

    add status message for completion of setup.sh

--- setup.sh
+++ setup.sh
@@ -22,3 +22,5 @@ sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo usermod -aG docker $USER
+
+echo -e "\nDone! Please close this terminal window and start a new session."
